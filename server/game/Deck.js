f98e659b11b803a58a5983837948ef18f9f22263
M1 <164732456+TheGoat79@users.noreply.github.com>
2026-06-06 21:15:51 +0100

    Add Deck model

---
+++ server/game/Deck.js
@@ -0,0 +1,32 @@
+const Card = require('./Card');
+
+class Deck {
+  constructor() {
+    this.cards = [];
+    const suits = ['♠','♥','♦','♣'];
+    const ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+
+    for (const s of suits) {
+      for (const r of ranks) {
+        this.cards.push(new Card(s, r));
+      }
+    }
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  deal(players) {
+    let p = 0;
+    while (this.cards.length) {
+      players[p].hand.push(this.cards.pop());
+      p = (p + 1) % players.length;
+    }
+  }
+}
+
+module.exports = Deck;
