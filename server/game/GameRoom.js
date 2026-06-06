f6e916ba7f4a8a0ed067bd47a592aeb5cea0a33a
M1 <164732456+TheGoat79@users.noreply.github.com>
2026-06-06 21:16:14 +0100

    Add GameRoom core logic

---
+++ server/game/GameRoom.js
@@ -0,0 +1,51 @@
+const Deck = require('./Deck');
+const Player = require('./Player');
+
+class GameRoom {
+  constructor(roomCode) {
+    this.roomCode = roomCode;
+    this.players = [];
+    this.deck = null;
+    this.trick = [];
+    this.currentTurn = 0;
+    this.started = false;
+    this.scores = {};
+  }
+
+  addPlayer(id) {
+    if (this.players.length >= 4) return false;
+
+    const player = new Player(id);
+    this.players.push(player);
+    this.scores[id] = 0;
+
+    return true;
+  }
+
+  startGame() {
+    if (this.players.length !== 4) return false;
+
+    this.deck = new Deck();
+    this.deck.shuffle();
+    this.deck.deal(this.players);
+
+    this.started = true;
+    this.currentTurn = 0;
+
+    return true;
+  }
+
+  playCard(playerId, cardIndex) {
+    const playerIndex = this.players.findIndex(p => p.id === playerId);
+    if (playerIndex !== this.currentTurn) return null;
+
+    const card = this.players[playerIndex].playCard(cardIndex);
+    this.trick.push({ playerId, card });
+
+    this.currentTurn = (this.currentTurn + 1) % 4;
+
+    return card;
+  }
+}
+
+module.exports = GameRoom;
