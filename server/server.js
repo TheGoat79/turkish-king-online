27dc11bec16279fa533f73fe3068136c5d5ebb27
M1 <164732456+TheGoat79@users.noreply.github.com>
2026-06-06 21:18:36 +0100

    Wire Socket.IO game socket layer

--- server/server.js
+++ server/server.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const registerGameSocket = require('./sockets/gameSocket');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 app.use(express.static('client'));
 
-const rooms = {};
-
 io.on('connection', (socket) => {
-  socket.on('create-room', (roomCode) => {
-    rooms[roomCode] = { players: [socket.id] };
-    socket.join(roomCode);
-    socket.emit('room-created', roomCode);
-  });
-
-  socket.on('join-room', (roomCode) => {
-    if (!rooms[roomCode]) {
-      socket.emit('error-message', 'Room not found');
-      return;
-    }
-    rooms[roomCode].players.push(socket.id);
-    socket.join(roomCode);
-    io.to(roomCode).emit('player-count', rooms[roomCode].players.length);
-  });
+  console.log('Player connected:', socket.id);
+  registerGameSocket(io, socket);
 });
 
-server.listen(3000, () => console.log('Server running on 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('Server running on', PORT);
+});
